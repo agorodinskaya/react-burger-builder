0c156bf0153aaf30b52c6f87f9ade2533f13b761
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2020-10-20 14:42:09 +1100

    backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -67,6 +67,10 @@ class BurgerBuilder extends Component {
   purchaseHandler = () => {
     this.setState({purchasing : true})
   }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false})
+  }
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -76,7 +80,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing}><OrderSummary ingredients = {this.state.ingredients}/></Modal>
+        <Modal show={this.state.purchasing} modalClosed = {this.purchaseCancelHandler}><OrderSummary ingredients = {this.state.ingredients}/></Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           moreIngredient={this.addIngredientHandler}
